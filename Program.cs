8dc8f5023d979f836799db78ddb713caecdb4802
cslunetta <cslunetta@outlook.com>
2021-03-02 10:44:56 -0600

    commit to github

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Guess the Secret number! (1-100)");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("");
+
+            Console.Write("Guess a number: ");
+            string response = Console.ReadLine();
+            Console.WriteLine($"You guessed {response}!");
+        }
+    }
+}
